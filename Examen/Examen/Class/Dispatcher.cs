46ab93952eb6937923bd8f0601873007d36b86ec
pst <ruslan.ermeichook@gmail.com>
2017-03-12 11:25:14 +0300

    firs

---
+++ Examen/Examen/Class/Dispatcher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen.Class
+{
+    class Dispatcher
+    {
+        string firstName;              
+        int adjustment;
+
+        
+        public Dispatcher(string _name,int _adjustment)
+        {
+            FirstName = _name;
+            Adjustment = _adjustment;
+        }
+        #region field
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public int Adjustment
+        {
+            get { return adjustment; }
+            set { adjustment = value; }
+        }
+        #endregion
+    }
+}
